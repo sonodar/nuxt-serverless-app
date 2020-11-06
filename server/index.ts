f3982842595976b3b96752b28f5de3b8d1b048b2
園田 亮平 <ryohei-sonoda@m3.com>
2020-11-06 17:08:16 +0900

    add cors

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,13 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// express の動作確認用に API のエンドポイントを追加しておきます
 const apiRouter = express.Router()
+apiRouter.use(cors())
 apiRouter.post('/echo', (req, res) => res.json(req.body))
 app.use('/api', apiRouter)
 
